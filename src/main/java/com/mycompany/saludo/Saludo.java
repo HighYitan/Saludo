f8b9e5b47264953f6809207ceab3d2a0a5f0d200
HighYitan <sr.highyitan@gmail.com>
2023-10-01 03:33:27 +0200

    Primer commit

---
+++ src/main/java/com/mycompany/saludo/Saludo.java
@@ -0,0 +1,11 @@
+package com.mycompany.saludo;
+/**
+ *
+ * @author Yitan
+ */
+public class Saludo {
+
+    public static void main(String[] args) {
+        System.out.print("¡Hola Khristian!");
+    }
+}
